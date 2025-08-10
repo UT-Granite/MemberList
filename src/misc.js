0fb704bb0f7beda675ca6d55e2d52aa5f7ace856
UT-Granite <utgranite@gmail.com>
2025-08-11 01:29:11 +0900

    nc

--- src/misc.js
+++ src/misc.js
@@ -1,12 +1,12 @@
-const sessionID = sessionStorage.getItem('sessionID');
-console.log(sessionID);
+const allCookies = document.cookie;
+console.log(allCookies);
 const BodyElement = document.getElementsByTagName('body')[0];
-if (sessionID == null){
+if (allCookies == ""){
     let LoginButton = document.createElement('iframe');
     LoginButton.src = "https://script.google.com/macros/s/AKfycbxAWMDeN52QJUZbTEpnkYtVdfwZjH0SMil2o19ZkjrzNSCJ6HYlDZAv4Ld4D_HCHbqUMg/exec";
     BodyElement.appendChild(LoginButton);
 }else{
     let sessionID_info = document.createElement('p1');
-    sessionID_info.textContent = sessionID;
+    sessionID_info.textContent = allCookies;
     BodyElement.appendChild(sessionID_info);
 }
