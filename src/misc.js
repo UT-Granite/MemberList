b596197290f942cea065ef8632067bd8cc37bd43
UT-Granite <utgranite@gmail.com>
2025-08-13 03:04:28 +0900

    v0.3.1

--- src/misc.js
+++ src/misc.js
@@ -10,6 +10,22 @@ window.addEventListener('message',function(e){
             let sessionID_info = document.createElement('p1');
             sessionID_info.textContent = sessionID;
             BodyElement.appendChild(sessionID_info);
+
+            fetch(`https://script.google.com/macros/s/AKfycbxAWMDeN52QJUZbTEpnkYtVdfwZjH0SMil2o19ZkjrzNSCJ6HYlDZAv4Ld4D_HCHbqUMg/exec?info=name&userHash=${userHash}&sessionID=${sessionID}`)
+                .then((response)=>response.json())
+                .then((data)=>{
+                    if (data.ok){
+                        const user_name = data.name;
+                        let user_nameElem = this.document.createElement('p1');
+                        user_nameElem.textContent = user_name;
+                        BodyElement.appendChild(user_nameElem);
+                    }else{
+                        let errElem = this.document.createElement('p1');
+                        errElem.textContent = data.error;
+                        BodyElement.appendChild(errElem);
+                    }
+                });
+
             break;
     }
 })
@@ -19,7 +35,14 @@ try{
         action:'Notification',
         message:'1',
     },'*');
+    
 }catch (e){
+    displayLogin();
+}
+
+
+
+function displayLogin(){
     let LoginButton = document.createElement('iframe');
     LoginButton.src = "https://script.google.com/macros/s/AKfycbxAWMDeN52QJUZbTEpnkYtVdfwZjH0SMil2o19ZkjrzNSCJ6HYlDZAv4Ld4D_HCHbqUMg/exec";
     BodyElement.appendChild(LoginButton);
