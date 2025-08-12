49034ca3b36f00e15fa1e4301ffc700f28efd8fe
UT-Granite <utgranite@gmail.com>
2025-08-12 14:35:04 +0900

    nc

--- src/misc.js
+++ src/misc.js
@@ -1,12 +1,18 @@
-const allCookies = document.cookie;
-console.log(allCookies);
 const BodyElement = document.getElementsByTagName('body')[0];
-if (allCookies == ""){
-    let LoginButton = document.createElement('iframe');
-    LoginButton.src = "https://script.google.com/macros/s/AKfycbxAWMDeN52QJUZbTEpnkYtVdfwZjH0SMil2o19ZkjrzNSCJ6HYlDZAv4Ld4D_HCHbqUMg/exec";
-    BodyElement.appendChild(LoginButton);
-}else{
-    let sessionID_info = document.createElement('p1');
-    sessionID_info.textContent = allCookies;
-    BodyElement.appendChild(sessionID_info);
-}
+let sessionID;
+let LoginButton = document.createElement('iframe');
+LoginButton.src = "https://script.google.com/macros/s/AKfycbxAWMDeN52QJUZbTEpnkYtVdfwZjH0SMil2o19ZkjrzNSCJ6HYlDZAv4Ld4D_HCHbqUMg/exec";
+BodyElement.appendChild(LoginButton);
+
+window.addEventListener('message',function(e){
+    switch (e.data.action){
+        case 'SyncMessage':
+            sessionID = e.data.message;
+            LoginButton.remove();
+            let sessionID_info = document.createElement('p1');
+            sessionID_info.textContent = sessionID;
+            BodyElement.appendChild(sessionID_info);
+            break;
+    }
+})
+
